9ec9e805260dfe75647c99591e69b23e4abd6807
xiaolx29 <89908869+xiaolx29@users.noreply.github.com>
2024-05-06 10:02:43 +0000

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import pytest
+from reattempt import StandardReAttempt
+
+Class Test_ReAttempt():
+  def test_success():
+    sra = StandardReAttempt()
+    result = sra.run(lambda : return 1)
+    assert result == (True, 1)
+  
+  def test_exception():
+    sra = StandardReAttempt()
+    result = sra.run(lambda : raise Exception('exception')
+    assert result == (False, None)
