4c3fab02df343e0232d609577921f3a3f9b7fe72
xiaolx29 <89908869+xiaolx29@users.noreply.github.com>
2024-05-06 10:10:37 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 import pytest
 from reattempt import StandardReAttempt
 
-Class Test_ReAttempt():
-  def test_success():
-    sra = StandardReAttempt()
-    result = sra.run(lambda : return 1)
-    assert result == (True, 1)
-  
-  def test_exception():
-    sra = StandardReAttempt()
-    result = sra.run(lambda : raise Exception('exception')
-    assert result == (False, None)
+def test_success():
+  sra = StandardReAttempt()
+  result = sra.run(lambda : return 1)
+  assert result == (True, 1)
+
+def test_exception():
+  sra = StandardReAttempt()
+  result = sra.run(lambda : raise Exception('exception')
+  assert result == (False, None)
